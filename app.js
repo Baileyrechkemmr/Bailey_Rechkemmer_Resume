e34db65cd6909c64500b0e51af7380db180f3f0b
baileyrechkemmer <rechkemmer3@gmail.com>
2023-02-08 17:00:22 -0600

    carasel html/js/css are all done just need to add rest of img and fix any bugs that ocur. 2 paragrafes done on my bio html

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(() => {
-
+///////////resume functions
 // farway hide/show function
 $('.far').on('click', function () {
 	$('.infoF').toggleClass("hidden");
@@ -20,5 +20,43 @@ $('.pez').on('click', function () {
 $('.cas').on('click', function () {
 	$('.infoC').toggleClass('hidden');
 });
+////////////home page carasel functions///////////
 
+console.log('corgi carousel connected to app.js!');
+
+	let currentImgIndex = 0;
+	let numOfImages = $('.carousel-images').children().length - 1;
+
+	$('.next').on('click', () => {
+		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none');
+		//  currentImgIndex++;
+
+		if (currentImgIndex < numOfImages) {
+			currentImgIndex++;
+		} else {
+			currentImgIndex = 0;
+		}
+
+		$('.carousel-images')
+			.children()
+			.eq(currentImgIndex)
+			.css('display', 'block');
+
+		// stuff will go here
+	});
+	$('.previous').on('click', () => {
+		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none');
+		if (currentImgIndex > 0) {
+			currentImgIndex--;
+		} else {
+			currentImgIndex = numOfImages;
+		}
+
+		$('.carousel-images')
+			.children()
+			.eq(currentImgIndex)
+			.css('display', 'block');
+
+		// stuff will go here
+	});
 });
